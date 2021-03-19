f7c44006c3275cf095cddb36bb705d5a029d95b2
Leon Bittner <leon.bittner@mailbox.org>
2021-03-19 18:27:55 +0100

    fixes

--- index.py
+++ index.py
@@ -60,7 +60,7 @@ def funktionsaufruf(eingabe):
             TicTacToe = TIC_TAC_TOE.HendriksTicTacToe(nutzername)
             TicTacToe.starte_Spiel()
         if i in Spiel2:
-            #Starte NewsModul.
+            #Marius_Pong.main()
 
     #------------------------Marius----------------------
             covidzahlen.main()
