75c62c6b3e8cb14ba561448e031f9acecf16e02f
Hendrik-s <hendrik.schumann@leibniz-fh.de>
2021-03-21 16:45:17 +0100

    Main Schleifenproblem gefixt
    Update Main

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 
 """
-Index.py erstellt von Mika Selent und Hendrik Schumann
+Index.py erstellt von Mika Selent, Leon Bittner und Hendrik Schumann
 
 Über die Index ist es mglich, die einzelnen Module zu öffnen.
 
@@ -13,6 +13,7 @@ from Mika import news
 from Bao import wetterfunktion
 from Endres import Sport_Kalender_Einkaufen
 from Marius import Marius_Pong
+global status 
 status = 1
 
 print("Herzlichen Willkommen zum neuen Chatbot.")
@@ -47,35 +48,45 @@ def funktionsaufruf(eingabe):
         if i in Covid:
             #Starte CovidModul.
             covidzahlen.main(nutzername)
+            return
         if i in News:
             #Starte NewsModul.
             news.news_main()
+            return
         if i in Spiel:
             if input("Tic Tac Toe (Ja/Nein)") == "Ja":
                 TicTacToe = TIC_TAC_TOE.HendriksTicTacToe(nutzername)
                 TicTacToe.starte_Spiel()
+                return
             else:
                 TicTacToe.starte_Spiel()
+                return
     #------------------------Marius----------------------
         if i in Spiel1:
             #Starte TicTacToeModul. 
             TicTacToe = TIC_TAC_TOE.HendriksTicTacToe(nutzername)
             TicTacToe.starte_Spiel()
+            return
         if i in Spiel2:
            Marius_Pong.spielAblauf()
+           return
     #------------------------Marius----------------------
         if i in Wetter:
             #Starte NewsModul.
             wetterfunktion.wetteraufruf()
+            return
         if i in Kalendar:
             #Starte KalenderModul.
             Sport_Kalender_Einkaufen.Kalender()
+            return
         if i in Einkaufsliste:
             #Starte Einkaufslistenmodul hinzufügen/anzeigen
             Sport_Kalender_Einkaufen.addEL()
+            return
         if i in loescheEinkaufsliste:
             #Starte Einkaufslistenmodul löschen
             Sport_Kalender_Einkaufen.delEL()
+            return
         else:
             print("Das kenne ich leider noch nicht")
 
@@ -84,6 +95,10 @@ def funktionsaufruf(eingabe):
 eingabe = input("Was möchtest du ausprobieren?\n ")
 funktionsaufruf(eingabe)
 
-if status == 1:
-    eingabe = input("Kann ich dir noch anders helfen?\n Bitte wähle aus meinen Funktionalitäten:\n - Aktuelle Covid-19-Zahlen (Länder o. Bundesländer)​ \n - Aktuelle News/Schlagzeilen​ \n - Live Wetter / Wetteraussichten​ \n - Kalenderinformationen​ \n - Ihre Einkaufsliste \n - Der Arcade-Klassiker Pong​ \n - Das Legendäre TicTacToe \n - Wähle 'bye' zum Beenden. \n")
-    funktionsaufruf(eingabe)
+
+def main():
+    while status == 1:
+        eingabe = input("Kann ich dir noch anders helfen?\n Bitte wähle aus meinen Funktionalitäten:\n - Aktuelle Covid-19-Zahlen (Länder o. Bundesländer)​ \n - Aktuelle News/Schlagzeilen​ \n - Live Wetter / Wetteraussichten​ \n - Kalenderinformationen​ \n - Ihre Einkaufsliste \n - Der Arcade-Klassiker Pong​ \n - Das Legendäre TicTacToe \n - Wähle 'bye' zum Beenden. \n")
+        funktionsaufruf(eingabe)
+        
+main()
